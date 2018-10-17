7589f1f7690a161274f4cb6c235ed5c8b8aefea0
hu553in <hasanluckyman@gmail.com>
2018-10-17 22:40:22 +0600

    implemented errors/exceptions handling and bufferedReader closing in Main class

--- Formatter/src/it/sevenbits/formatter/Main.java
+++ Formatter/src/it/sevenbits/formatter/Main.java
@@ -7,14 +7,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(args[0]));
+    private static List<Character> readTextFromFile (String path) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
         List<Character> text = new ArrayList<>();
 
         int character = bufferedReader.read();
         while (character != -1) {
             text.add((char)character);
             character = bufferedReader.read();
+        }
+
+        bufferedReader.close();
+        return text;
+    }
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("Not enough arguments (expected 1).");
+        }
+
+        List<Character> text;
+
+        try {
+            text = readTextFromFile(args[0]);
+        } catch (IOException e) {
+            System.err.println("An instance of IOException was thrown.");
+            return;
         }
 
         Formatter formatter = new Formatter();
