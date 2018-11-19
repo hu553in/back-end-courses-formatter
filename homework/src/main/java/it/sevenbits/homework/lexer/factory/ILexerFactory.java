acaf6f9bdcc8788d19536beb1d584d02ce28ae47
hu553in <hasanluckyman@gmail.com>
2018-11-20 02:27:37 +0600

    changed JavaDoc ILexerFactory interface and LexerFactory class

--- homework/src/main/java/it/sevenbits/homework/lexer/factory/ILexerFactory.java
+++ homework/src/main/java/it/sevenbits/homework/lexer/factory/ILexerFactory.java
@@ -4,15 +4,16 @@ import it.sevenbits.homework.io.reader.IReader;
 import it.sevenbits.homework.lexer.ILexer;
 
 /**
- * Interface that declares a functionality for creating of {@link ILexer} instances
+ * Interface that declares a functionality for creating of various {@link ILexer} instances
  * using "Factory method" design pattern.
  */
 public interface ILexerFactory {
     /**
-     * Method that creates optional {@link ILexer} instance in accordance with passed {@link IReader} implementation.
+     * Method that creates an instance of one of {@link ILexer} interface implementations
+     * in accordance with passed {@link IReader} implementation.
      *
-     * @param reader Instance of {@link IReader} which is used to select, create,
-     *               and initialize one of the implementations of {@link ILexer} interface.
+     * @param reader Instance of {@link IReader} which is used to select, create, and initialize
+     *               one of the implementations of {@link ILexer} interface.
      *
      * @throws LexerFactoryException Exception that can be thrown during the method work.
      *
